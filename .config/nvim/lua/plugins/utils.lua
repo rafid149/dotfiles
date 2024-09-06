a093b7bc25bec51080e1311e9816efcec59df78f
rafid149 <85733811+rafid149@users.noreply.github.com>
2024-09-06 11:09:43 +0600

    Create utils.lua

---
+++ .config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "aserowy/tmux.nvim",
+    -- event = "VeryLazy",
+    keys = {
+      "<C-h>", "<C-j>", "<C-k>", "<C-l>",
+      "<M-h>", "<M-j>", "<M-k>", "<M-l>",
+    },
+    opts = {},
+  },
+}
