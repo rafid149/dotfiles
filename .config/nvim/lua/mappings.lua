c075b0b8e52a9b7e2d441210c91a3e1ff6456acf
rafid149 <85733811+rafid149@users.noreply.github.com>
2024-09-06 11:10:49 +0600

    Create mappings.lua

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,11 @@
+-- require "nvchad.mappings"
+require("nvmaps")
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
